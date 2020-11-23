262f06526306e208517992ae8824443d8a295fa7
blen_meku <mekuriablen@gmail.com>
2020-11-22 23:38:58 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById('new-task-description');
+  const newTaskAppear = document.getElementById("tasks");  
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+
+function createNewTask(event) {
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+  appendNewTask(newTask);
+  event.preventDefault();  
+};
+
+
+function appendNewTask(task) {
+  document.getElementById("tasks").appendChild(task);
+};
